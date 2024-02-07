03e119821ee3b6fcd3c3d6b30c14a39ca82de3a8
iampitrus <ogbonnapete210@gmail.com>
2024-02-07 16:35:38 +0100

    ADD: server

---
+++ client/App.js
@@ -0,0 +1,11 @@
+import { Provider } from "react-redux";
+import Navigation from "./Navigation";
+import { store } from "./store";
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
+}
